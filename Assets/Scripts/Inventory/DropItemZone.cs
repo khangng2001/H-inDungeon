ab60698d8551b42e9ef660679587bccab298c8a1
MinhXavia <109792857+MinhXavia@users.noreply.github.com>
2023-09-18 15:54:53 +0700

    update
    add lightning for NPC
    update DropItemUI
    Show/Hide DropItemUI when drag item

--- Assets/Scripts/Inventory/DropItemZone.cs
+++ Assets/Scripts/Inventory/DropItemZone.cs
@@ -1,14 +1,33 @@
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class DropItemZone : MonoBehaviour, IDropHandler
 {
+    public static DropItemZone instance;
+
+    private void Awake()
+    {
+        Hide();
+        instance = this;
+    }
+
+    public void Show()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        this.gameObject.SetActive(false);
+    }
+
     public void OnDrop(PointerEventData eventData)
     {
         InventoryItem inventoryItem = eventData.pointerDrag.GetComponent<InventoryItem>();  //get the item
 
         InventoryManager.instance.DropItemOnGround(inventoryItem);
         InventoryManager.instance.DropItem(inventoryItem);
+
+        Hide();
     }
 }
