abcbd095a54e0774362aea8a4fdb64867053cc5f
MinhXavia <109792857+MinhXavia@users.noreply.github.com>
2023-09-20 10:14:26 +0700

    add ItemNameUI

---
+++ Assets/Scripts/Inventory/ItemNameUI.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class ItemNameUI : MonoBehaviour
+{
+    public void Show()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+}
